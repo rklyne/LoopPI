52bcadef09f2af0cb3324216846e06d9c634477b
Ronan Klyne <github@rklyne.net>
2015-05-15 17:58:55 +0000

    improved start script

--- start.sh
+++ start.sh
@@ -1,4 +1,15 @@
-jackd  --realtime -p8 -t100000 -d alsa -dhw:1 -p512 -n3 -H -M -z none -s&
-sleep 3
+#!/bin/bash
+
+service ntp stop
+service triggerhappy stop
+service dbus stop
+sudo mount -o remount,size=256M /dev/shm
+
+BUFSIZE=2048
+jackd  --realtime -p8 -t100000 -d alsa -dhw:1 -p${BUFSIZE} -n2 -H -M -z none -s&
+# jack_bufsize $BUFSIZE
+sleep 1.5
 ~/chuck --in1 Looper/looper-pedal.ck
+
+killall jackd
 
