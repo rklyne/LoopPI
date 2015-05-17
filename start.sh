45815e209a2895df3bbe3f7732aa93c65bff1656
Ronan Klyne <github@rklyne.net>
2015-05-17 10:30:41 +0000

    One shot startup script

--- start.sh
+++ start.sh
@@ -5,10 +5,20 @@ service triggerhappy stop
 service dbus stop
 sudo mount -o remount,size=256M /dev/shm
 
-BUFSIZE=512
-jackd  --realtime -p8 -t100000 -d alsa -dhw:1 -p${BUFSIZE} -n2 -H -M -z none -s&
-# jack_bufsize $BUFSIZE
-sleep 1.5
+BUF_SIZE=256
+BUF_NUM=4
+JACK_OPTS=
+JACK_ALSA_OPTS=
+# Mono in:
+JACK_ALSA_OPTS="${JACK_ALSA_OPTS} -i1"
+jackd  --realtime -P20 -p8 -t100000 ${JACK_OPTS} -d alsa -dhw:1 -p${BUF_SIZE} -n${BUF_NUM} -H -M ${JACK_ALSA_OPTS} -z none -s&
+# jack_bufsize $BUF_SIZE
+sleep 1
+(
+    sleep 1;
+    sudo renice -20 `pidof chuck`;
+    echo "niced chuck, pid ", `pidof chuck`
+) &
 ~/chuck --in1 Looper/looper-pedal.ck
 
 killall jackd
