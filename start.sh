584c8f0c29e985ae813aed41387ee0c0ddcf2324
Ronan Klyne <github@rklyne.net>
2015-05-17 11:44:48 +0000

    More startup parameter tuning.

--- start.sh
+++ start.sh
@@ -5,13 +5,14 @@ service triggerhappy stop
 service dbus stop
 sudo mount -o remount,size=256M /dev/shm
 
+RATE=44100
 BUF_SIZE=256
-BUF_NUM=4
+BUF_NUM=3
 JACK_OPTS=
 JACK_ALSA_OPTS=
 # Mono in:
 JACK_ALSA_OPTS="${JACK_ALSA_OPTS} -i1"
-jackd  --realtime -P20 -p8 -t100000 ${JACK_OPTS} -d alsa -dhw:1 -p${BUF_SIZE} -n${BUF_NUM} -H -M ${JACK_ALSA_OPTS} -z none -s&
+jackd  --realtime -P1 -p8 -t100000 ${JACK_OPTS} -d alsa -dhw:1 -p${BUF_SIZE} -n${BUF_NUM} -H -M ${JACK_ALSA_OPTS} -z none -s --rate ${RATE} &
 # jack_bufsize $BUF_SIZE
 sleep 1
 (
@@ -19,7 +20,7 @@ sleep 1
     sudo renice -20 `pidof chuck`;
     echo "niced chuck, pid ", `pidof chuck`
 ) &
-~/chuck --in1 Looper/looper-pedal.ck
+~/chuck --in1 --srate:${RATE} Looper/looper-pedal.ck
 
 killall jackd
 
