6e21fe489c3a8bd01af1c8793f2f45b02c34e8c9
Ronan Klyne <github@rklyne.net>
2015-05-19 19:46:17 +0000

    Improved install script - works in full now

--- install-script.sh
+++ install-script.sh
@@ -4,13 +4,16 @@
 ##   cd LoopPI
 ##   ./install-script.sh
 
+sudo apt-get update
+sudo apt-get install -y zsh vim exuberant-ctags chuck make python-pip cython python-dev liblo-dev jackd python2.6-dev python2.7-dev bison python-dev flex htop libjack-jackd2-dev libsndfile-dev libasound2-dev
 
-apt-get install -y zsh vim exuberant-ctags chuck make python-pip cython python-dev liblo-dev jackd python2.6-dev python2.7-dev
+mkdir ~/build
 (
-wget http://das.nasophon.de/download/pyliblo-0.9.2.tar.gz ;
-tar -pvzxf *.tar.gz ;
-cd pyliblo*;
-python setup.py install
+    cd ~/build ;
+    wget http://das.nasophon.de/download/pyliblo-0.9.2.tar.gz ;
+    tar -pvzxf *.tar.gz ;
+    cd pyliblo* ;
+    python setup.py install ;
 )
 
 (
@@ -18,18 +21,30 @@ python setup.py install
     ./prepare-runtime.sh;
 )
 
-# # JACK repo
-# wget -O - http://rpi.autostatic.com/autostatic.gpg.key | sudo apt-key add -
-# sudo wget -O /etc/apt/sources.list.d/autostatic-audio-raspbian.list http://rpi.autostatic.com/autostatic-audio-raspbian.list
-# sudo apt-get update
+# JACK repo
+# redundant - in core raspbian now
+# (
+#     sudo wget -O - http://rpi.autostatic.com/autostatic.gpg.key | sudo apt-key add -
+#     sudo wget -O /etc/apt/sources.list.d/autostatic-audio-raspbian.list http://rpi.autostatic.com/autostatic-audio-raspbian.list
+#     sudo apt-get update
+# )
+
 
 # ADD CHUCK BUILD
-wget http://chuck.cs.princeton.edu/release/files/chuck-1.3.5.1.tgz
-tar -pvzxf chuck-1.3.5.1.tgz
 (
-    cd chuck-1.3.5.1/src;
-    make linux-jack
-    make install
+    cd ~/build;
+    wget http://chuck.cs.princeton.edu/release/files/chuck-1.3.5.1.tgz ;
+    tar -pvzxf chuck-1.3.5.1.tgz ;
+    cd chuck-1.3.5.1/src ;
+    make -j 6 linux-jack ;
+    cp ./chuck ~/chuck ;
 )
+
+
+#### configure dbus
+
+sudo sed -i "s|</busconfig>|<policy user=\"pi\"><allow own=\"org.freedesktop.ReserveDevice1.Audio1\"/></policy></busconfig>|" /etc/dbus-1/system.conf
+sudo service dbus restart
+
 
 
