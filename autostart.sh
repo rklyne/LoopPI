528894098f23165c314fa586cc91654048ba57d3
Ronan Klyne <ronan.klyne@virtualstock.co.uk>
2015-07-31 21:13:17 +0000

    Auto start script

--- autostart.sh
+++ autostart.sh
@@ -1,4 +1,9 @@
 cd ~/LoopPI
 sudo python buttons.py &
-./start.sh
+./start.sh | tee -a ~/looper.log
+echo "exited - cleaning up"
+ps aux | grep button | grep python | grep "on[s]" | awk '{print $2}' | xargs sudo kill
+killall chuck
+killall jackd
+echo "Ended"
 
